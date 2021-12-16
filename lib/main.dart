e810d348abb9ca7c3dfb027babb9765e06cd5cf6
Cristian Eduardo Galeano Acosta <cristian.galeano@globant.com>
2021-12-16 14:09:06 -0500

    code refactored on splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-final Color mainColor = Color(0xFFFF5656);
+
+const Color mainColor = Color(0xFFFF5656);
 
 void main() {
   runApp(
@@ -11,28 +11,55 @@ void main() {
   );
 }
 
+
+class _SplashIcon extends StatelessWidget {
+
+  const _SplashIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const Align(
+      alignment: Alignment.center,
+      child: Icon(Icons.terrain, color: Colors.white, size: 90),
+    );
+  }
+}
+
+
+class _SplashSpinner extends StatelessWidget{
+
+  const _SplashSpinner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+            margin: const EdgeInsets.only(bottom: 80),
+            child: const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            )
+        )
+    );
+  }
+}
+
+
 class SplashPage extends StatelessWidget {
+
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
     return Container(
         color: mainColor,
         child: Stack(
-          children: [
-            const Align(
-              alignment: Alignment.center,
-              child: Icon(Icons.terrain, color: Colors.white, size: 90),
-            ),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  margin: const EdgeInsets.only(bottom: 80),
-                  child: const CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                  )
-                )
-            )
+          children: const [
+            _SplashIcon(),
+            _SplashSpinner(),
           ],
         )
     );
