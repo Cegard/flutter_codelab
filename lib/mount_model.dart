a4c15e2c7194f55eb441a7de634f9e778ba87bd1
Eduardo Galeano <cristian.galeano@globant.com>
2021-12-22 14:01:44 -0500

    mounts list view widget added

---
+++ lib/mount_model.dart
@@ -0,0 +1,17 @@
+
+
+class MountModel {
+
+  String path;
+  String name;
+  String description;
+  String location;
+
+  MountModel({
+    this.path = "",
+    this.name = "",
+    this.description = "",
+    this.location = ""
+  });
+}
+
